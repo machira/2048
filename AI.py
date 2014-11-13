eb032650b1070608764aef468df14aa862852e41
Raymond Machira <raymond.machira@gmail.com>
2014-11-13 10:57:34 -0500

    list of possible moves and their probability

--- AI.py
+++ AI.py
@@ -12,6 +12,12 @@ def evaluate_move(board, move, depth=4):
     if depth < 0:
         return board.score
     score = 0
+    best_move = 0
+    possible_new_cells = Board.possible_new_pieces()
+    for piece, cell, probability in possible_new_cells:
+
+    for move in Game.MOVES:
+
     evaluate_move(board, depth=depth-1)
 
 
