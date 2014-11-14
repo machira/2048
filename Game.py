5910902dc0bfa08a314f5a947d5954fe7b1e309d
Raymond Machira <raymond.machira@gmail.com>
2014-11-13 20:46:19 -0500

    tests pass, instrumentation added.

--- Game.py
+++ Game.py
@@ -3,52 +3,168 @@ __author__ = 'Raymond Machira <raymond.machira@gmail.com>'
 # ##
 ### A 2048 solver, using MiniMax with A-B pruning.
 ###
-from operator import itemgetter
-
+import random, sys
 # move across all unoccupied cells
 # Check for any similar cells and collapse sequentially.
 # Move across empty cells once more
 
 
-class Game():
-    # left right up down
-    LEFT = -1
-    RIGHT = 1
-    UP = -4
-    DOWN = 4
-    MOVES = [LEFT, RIGHT, UP, DOWN]
+# left right up down
+LEFT = -1
+RIGHT = 1
+UP = -4
+DOWN = 4
+MOVES = [LEFT, RIGHT, UP, DOWN]
+BOARD_SIZE = 4
+WINNING_TILE = 1024
+WEIGHTED_PIECES = [(2, 50), (4, 25), (8, 12.5), (16, 6.25), (32, 6.25)]
+# WEIGHTED_PIECES = [(2, 100)]#, (4, 25), (8, 12.5), (16, 6.25), (32, 6.25)]
 
-    def make_move(self,board, move, collapse=True):
-        """
-        :param board:
-        :param move:
-        :return:
-        """
-        limits = []
+def make_move(board, move):
+    """
+    :param board: the game board on which to make a move
+    :param move: the move to be made
+    :return: the board, with the move made, and the score populated as the last item.
+    """
+    limits = []
+    # number of smashes for this move
+    number_smashes = 0
+    score_increment = 0
 
-        if move == self.LEFT:
-            limits = [board.board_size * i for i in range(0,board.board_size)]
-        elif move == self.RIGHT:
-            limits = [(board.board_size * i)-1 for i in range(1,board.board_size+1)]
-        elif move == self.UP:
-            limits =  range(0,board.board_size)
-        elif move == self.DOWN:
-            limits = [i for i in range(board.board_size * (board.board_size-1), board.board_size*board.board_size)]
+    if move == LEFT:
+        limits = [BOARD_SIZE * i for i in range(0, BOARD_SIZE)]
+    elif move == RIGHT:
+        limits = [(BOARD_SIZE * i)-1 for i in range(1, BOARD_SIZE+1)]
+    elif move == UP:
+        limits =  range(0, BOARD_SIZE)
+    elif move == DOWN:
+        limits = [i for i in range(BOARD_SIZE * (BOARD_SIZE-1), BOARD_SIZE*BOARD_SIZE)]
 
 
-        # empty_cells = board.empty_cells()
-        occupied_cells = board.occupied_cells()
-        # sort the cells to start at the right end
-        occupied_cells.sort(reverse= move > 0)
-        for cell in occupied_cells:
-            move_to = cell
-            while(move_to not in limits):
-                if board.board[cell+move] == 0 or board.board[cell+move] == board.board[cell]:
-                    move_to += move
-                else:
-                    break
-            if move_to != cell:
-                board.board[move_to] +=  board.board[cell]
-                board.board[cell]  = 0 # make the old cell empty
+    # empty_cells = board.empty_cells()
+    occupied = occupied_cells(board)
+    # sort the cells to start at the right end
+    occupied.sort(reverse= move > 0)
+    for cell in occupied:
+        move_to = cell
+        while move_to not in limits:
+            if board[move_to+move] == 0:
+                move_to += move
+            # neighbouring cell is actually the same as this one, a smash can take place/we can move one more.
+            elif board[move_to+move] == board[cell]:
+                number_smashes += 1
+                move_to += move
+                # also increment our score. I use addition instead of multiplication- a small optimization.
+                score_increment += board[cell]+board[cell]
+            else:
+                break
+        if move_to != cell:
+            board[move_to] +=  board[cell]
+            board[cell]  = 0 # make the old cell empty
 
-        return board
+    return board, number_smashes, score_increment
+
+
+def random_empty_cell(board):
+    empty_cell = empty_cells(board)
+    if len(empty_cell):
+        return random.choice(empty_cells)
+
+
+def empty_cells(board):
+    return [i for i in range(0, len(board)) if board[i] == 0]
+
+def occupied_cells(board):
+    return [i for i in range(0,len(board)) if board[i] != 0]
+
+def game_lost(board):
+    return len(empty_cells(board)) == 0
+
+
+def game_won(board):
+    return max(board) >= WINNING_TILE
+
+
+## simply returns a possible block
+def random_block():
+    total = sum(w for c, w in WEIGHTED_PIECES)
+    r = random.uniform(0, total)
+    upto = 0
+    for c, w in WEIGHTED_PIECES:
+        if upto + w > r:
+            return c
+        upto += w
+
+def new_board(board_size=BOARD_SIZE):
+    board = [0]*(board_size*board_size)
+    return random_block_spawn(board)
+
+def random_block_spawn(board):
+    cell = random.choice(empty_cells(board))
+    board[cell] = random_block()
+    return board
+
+def make_random_move(board):
+    move = random.choice(MOVES)
+    return make_move(board,move)
+
+def make_max_scoring_move(board):
+    best_move = MOVES[0]
+    max_score = 0
+    for move in MOVES:
+        board, number_smashes, score_increment  = make_move(board[:],move)
+        if score_increment > max_score:
+            best_move = move
+
+    return make_move(board,best_move)
+
+# def evaluate_move(board, move, depth=4):
+#     if board.game_won: return float("Infinity")
+#     # this game ends in loss
+#     elif board.game_over: return float("-Infinity")
+#     # stops recursion
+#     elif depth < 0:
+#         return board.score
+#
+#     possible_new_cells = possible_new_pieces()
+#     for piece, cell, probability in possible_new_cells:
+#
+#     for move in MOVES:
+#
+#         evaluate_move(board, depth=depth-1)
+
+
+if __name__ == '__main__':
+    num_iterations = 10000
+
+    strategies = [(make_max_scoring_move,'max_scoring_move.csv'), (make_random_move,'random_move.csv'),
+                  (make_max_scoring_move, 'max_scoring_move.csv')
+
+                ]
+
+    if len(sys.argv) > 1:
+        num_iterations = int(sys.argv[2])
+
+    for strategy,file_name in strategies:
+        # prepare results file with appropriate headings
+        with open(file_name, 'w') as results_file:
+            results_file.write("Game_Won,Max_block,Num_moves,Score\n")
+
+        for iter in range(0,num_iterations):
+            board = new_board(board_size=4)
+            num_moves_made = score = smashes = 0
+            print 'completed ',iter
+            while not game_lost(board):
+                board, number_smashes, score_increment = strategy(board)
+                score += score_increment
+                smashes += number_smashes
+                num_moves_made += 1
+                board = random_block_spawn(board)
+                # if game_won(board):
+                #     break
+
+            win = game_won(board)
+            max_block = max(board)
+
+            with open(file_name, 'a') as results_file:
+                results_file.write("{0},{1},{2},{3},{4}\n".format(win, max_block, num_moves_made, score, smashes))
--- tests.py
+++ tests.py
@@ -2,50 +2,41 @@ __author__ = 'macharir'
 
 import unittest
 
-from Board import Board
-from Game import Game
+import Game
 
 
 class MyTestCase(unittest.TestCase):
-    game = Game()
+    game = Game
 
     def test_move_left(self):
-        board1 = Board(board_size=4)
         b = [0]*16
         b[1] = 2
         b[2] = 2
-        board1.set_board(b)
 
-        self.game.make_move(board1,self.game.LEFT)
-        self.assertTrue(board1.board==[4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+        self.game.make_move(b,self.game.LEFT)
+        self.assertTrue(b==[4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 
     def test_move_right(self):
-        board2 = Board()
         b = [0]*16
         b[1] = 2
         b[2] = 2
-        board2.set_board(b)
 
-        self.game.make_move(board2,self.game.RIGHT)
-        self.assertTrue(board2.board==[0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0])
+        self.game.make_move(b,self.game.RIGHT)
+        self.assertTrue(b==[0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0])
 
     def test_move_up(self):
-        board3 = Board()
         b = [0]*16
         b[1] = 2
         b[5] = 2
-        board3.set_board(b)
-        self.game.make_move(board3,self.game.UP)
-        self.assertTrue(board3.board==[0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+        self.game.make_move(b,self.game.UP)
+        self.assertTrue(b==[0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 
     def test_move_down(self):
-        board4 = Board()
         b = [0]*16
         b[1] = 2
         b[5] = 2
-        board4.set_board(b)
-        self.game.make_move(board4,self.game.DOWN)
-        self.assertTrue(board4.board==[0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0])
+        self.game.make_move(b,self.game.DOWN)
+        self.assertTrue(b==[0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0])
 
 
 if __name__ == '__main__':
